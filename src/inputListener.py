5282039bff0f2dd77fb4fb7fc11e7cee9fb78d1a
Tobias Albert <tobias.albert@hetzner.com>
2021-03-25 06:31:39 +0100

    current state

--- src/inputListener.py
+++ src/inputListener.py
@@ -1,3 +1,6 @@
+from datetime import datetime, time as dtime
+from pytz import timezone
+
 from CASlibrary import Config, Logger, RedisMB
 from CASlibrary.constants import AlertType
 
@@ -33,7 +36,7 @@ class InputListener:
             self.logger.info("ZVEI does not match to any trigger. Stopping.")
             return
 
-        actions = []
+        actions = {}
         for trigger in triggers:
             if not self._isTriggerActive(trigger):
                 self.logger.info("Trigger {} is not active", trigger["name"])
@@ -41,17 +44,81 @@ class InputListener:
             if "action" not in trigger or len(trigger["action"]) == 0:
                 self.logger.info("Trigger {} has no actions", trigger["name"])
                 continue
+
+            template = {"trigger_name": trigger["name"], "zvei": zvei}
             self.logger.info("Adding actions of trigger {} to queue", trigger["name"])
             for action in trigger["action"]:
                 if action not in actions:
                     self.logger.debug("Adding action {} of trigger {} to queue", action, trigger["name"])
-                    actions.append(action)
+                    actions[action] = template
                 else:
                     self.logger.debug("Omit action {} of trigger {} as it is already in queue", action, trigger["name"])
 
-
     def _isTriggerActive(self, trigger):
-        pass
+        def _isActiveTimeNow(data, name):
+            now = datetime.now(timezone("Europe/Berlin"))
 
+            if isinstance(data, bool):
+                self.logger.debug("Trigger {} active config is bool. Returning it", name)
+                return data
+            for d in data:
+                self.logger.debug("Trigger {}: Checking value {} of type {}", name, d, type(d))
+                if isinstance(d, bool):
+                    self.logger.debug("Trigger {}: Found a bool in the active config. Returning it", name)
+                    return d
+                elif isinstance(d, int):
+                    if d == now.weekday():
+                        self.logger.debug("Trigger {}: Int does match to the current weekday. Return true", name)
+                        return True
+                elif isinstance(d, dict):
+                    if len(d) == 0:
+                        self.logger.debug("Trigger {}: Dict is empty. Ignore", name)
+                        continue
+                    for k, v in d.items():
+                        if k == "weekday":
+                            if v != now.weekday():
+                                continue
+                        elif k == "between":
+                            begin_time = dtime(v[0][0], v[0][1])
+                            end_time = dtime(v[1][0], v[1][1])
+                            if begin_time > now.time() or now.time() > end_time:
+                                continue
+                        else:
+                            self.logger.warning("Found unknown key {} with value {} in active of trigger", k, v)
+                    return True
+                else:
+                    self.logger.warning("Found unknown type {} with value {} in active of trigger", name, type(d), d)
+            return False
 
+        self.logger.debug("Check if trigger {} is active", trigger["name"])
+        inactive = False
+        data = []
+        if "active" not in trigger:
+            if "inactive" not in trigger:
+                self.logger.debug("Trigger {} has no active nor inactive key in config. So it es active",
+                                  trigger["name"])
+                return True
+            return not _isActiveTimeNow(data, trigger["name"])
+        else:
+            return _isActiveTimeNow(data, trigger["name"])
 
+    def _isActiveTimeNow(self, data, name):
+        now = datetime.now(timezone("Europe/Berlin"))
+
+        if isinstance(data, bool):
+            self.logger.debug("Trigger {} active config is bool. Returning it", name)
+            return data
+        for d in data:
+            self.logger.debug("Trigger {}: Checking value {} of type {}", name, d, type(d))
+            if isinstance(d, bool):
+                self.logger.debug("Trigger {}: Found a bool in the active config. Returning it", name)
+                return d
+            elif isinstance(d, int):
+                if d == now.weekday():
+                    self.logger.debug("Trigger {}: Int does match to the current weekday. Return true", name)
+                    return True
+            elif isinstance(d, dict):
+                pass
+            else:
+                self.logger.warning("Found unknown type {} with value {} in active of trigger", name, type(d), d)
+        return False
