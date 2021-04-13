a2b45d77bf4c47e5c8febdc4350e5191626ae576
TobsA13 <git@tobsa.wtf>
2021-04-13 13:50:13 +0200

    fixed logging
    Signed-off-by: TobsA13 <git@tobsa.wtf>

--- src/inputListener.py
+++ src/inputListener.py
@@ -39,40 +39,40 @@ class InputListener:
         actions = {}
         for trigger in triggers:
             if not self._isTriggerActive(trigger):
-                self.logger.info("Trigger {} is not active", trigger["name"])
+                self.logger.info("Trigger {} is not active".format(trigger["name"]))
                 continue
             if "action" not in trigger or len(trigger["action"]) == 0:
-                self.logger.info("Trigger {} has no actions", trigger["name"])
+                self.logger.info("Trigger {} has no actions".format(trigger["name"]))
                 continue
 
             template = {"trigger_name": trigger["name"], "zvei": zvei}
-            self.logger.info("Adding actions of trigger {} to queue", trigger["name"])
+            self.logger.info("Adding actions of trigger {} to queue".format(trigger["name"]))
             for action in trigger["action"]:
                 if action not in actions:
-                    self.logger.debug("Adding action {} of trigger {} to queue", action, trigger["name"])
+                    self.logger.debug("Adding action {} of trigger {} to queue".format(action, trigger["name"]))
                     actions[action] = template
                 else:
-                    self.logger.debug("Omit action {} of trigger {} as it is already in queue", action, trigger["name"])
+                    self.logger.debug("Omit action {} of trigger {} as it is already in queue".format(action, trigger["name"]))
 
     def _isTriggerActive(self, trigger):
         def _isActiveTimeNow(data, name):
             now = datetime.now(timezone("Europe/Berlin"))
 
             if isinstance(data, bool):
-                self.logger.debug("Trigger {} active config is bool. Returning it", name)
+                self.logger.debug("Trigger {} active config is bool. Returning it".format(name))
                 return data
             for d in data:
-                self.logger.debug("Trigger {}: Checking value {} of type {}", name, d, type(d))
+                self.logger.debug("Trigger {}: Checking value {} of type {}".format(name, d, type(d)))
                 if isinstance(d, bool):
-                    self.logger.debug("Trigger {}: Found a bool in the active config. Returning it", name)
+                    self.logger.debug("Trigger {}: Found a bool in the active config. Returning it".format(name))
                     return d
                 elif isinstance(d, int):
                     if d == now.weekday():
-                        self.logger.debug("Trigger {}: Int does match to the current weekday. Return true", name)
+                        self.logger.debug("Trigger {}: Int does match to the current weekday. Return true".format(name))
                         return True
                 elif isinstance(d, dict):
                     if len(d) == 0:
-                        self.logger.debug("Trigger {}: Dict is empty. Ignore", name)
+                        self.logger.debug("Trigger {}: Dict is empty. Ignore".format(name))
                         continue
                     for k, v in d.items():
                         if k == "weekday":
@@ -84,19 +84,18 @@ class InputListener:
                             if begin_time > now.time() or now.time() > end_time:
                                 continue
                         else:
-                            self.logger.warning("Found unknown key {} with value {} in active of trigger", k, v)
+                            self.logger.warning("Found unknown key {} with value {} in active of trigger".format(k, v))
                     return True
                 else:
-                    self.logger.warning("Found unknown type {} with value {} in active of trigger", name, type(d), d)
+                    self.logger.warning("Found unknown type {} with value {} in active of trigger".format(name, type(d), d))
             return False
 
-        self.logger.debug("Check if trigger {} is active", trigger["name"])
+        self.logger.debug("Check if trigger {} is active".format(trigger["name"]))
         inactive = False
         data = []
         if "active" not in trigger:
             if "inactive" not in trigger:
-                self.logger.debug("Trigger {} has no active nor inactive key in config. So it es active",
-                                  trigger["name"])
+                self.logger.debug("Trigger {} has no active nor inactive key in config. So it es active".format(trigger["name"]))
                 return True
             return not _isActiveTimeNow(data, trigger["name"])
         else:
